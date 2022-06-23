5c9eb982b3b2e1a07df31d46d6c8a7594b386933
VITALI BABENKA <vitalik1910293@gmail.com>
2022-06-24 00:24:11 +0300

    add app2.js

---
+++ app2.js
@@ -0,0 +1,30 @@
+
+const firstName = "Vitalik"; 
+
+
+
+// komments
+// const rot1
+// мутирование 
+//console.log('имя человека:'  + firstName + ', а возраст человека ')
+//const lastName = prompt('введите фамилию')
+//alert(firstName + ' ' + lastName)
+//операторы
+let currentYear = 2022
+const birthYear = 1993
+const age = currentYear - birthYear
+console.log(age)
+const a = 10
+const b = 2
+let c = 32
+
+console.log(a-b)
+console.log(a*b)
+console.log(currentYear++)
+console.log(c)
+const x = 42
+console.log(typeof x)
+const isReady = true
+if (isReady === true) {
+    console.log('Its all lessons today')
+}
