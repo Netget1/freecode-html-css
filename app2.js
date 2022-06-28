a4e1680a940e0b65c7712be5aedece3cdbf4dc41
VITALI BABENKA <vitalik1910293@gmail.com>
2022-06-28 21:06:01 +0300

    change app2.js

--- app2.js
+++ app2.js
@@ -1,30 +1,17 @@
 
-const firstName = "Vitalik"; 
 
 
-
-// komments
-// const rot1
-// мутирование 
-//console.log('имя человека:'  + firstName + ', а возраст человека ')
-//const lastName = prompt('введите фамилию')
-//alert(firstName + ' ' + lastName)
-//операторы
-let currentYear = 2022
-const birthYear = 1993
-const age = currentYear - birthYear
-console.log(age)
-const a = 10
-const b = 2
-let c = 32
-
-console.log(a-b)
-console.log(a*b)
-console.log(currentYear++)
-console.log(c)
-const x = 42
-console.log(typeof x)
-const isReady = true
-if (isReady === true) {
-    console.log('Its all lessons today')
+ // arrays
+ //const cars = ['toyota', 'bmw','renault']
+ //console.log(cars)
+ //console.log(cars.length)
+ //console.log(cars[0])
+ //cars[0] = 'mersedes';//
+//циклы
+const cars = ['bmw','renault','mazda', 'porshe'];
+//for (let i=0; i< cars.length; i++) {
+  // const car = cars[i];
+   //console.log(car)
+for (let car of cars) {
+   console.log(car)
 }
