837699a013c4c8ec51a1de9b0f77ccdfc0813b18
VITALI BABENKA <vitalik1910293@gmail.com>
2022-07-03 14:41:06 +0300

    add server.js file

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ let requestCount = 0
 const server = http.createServer((request, response) => 
 {
     requestCount++
-    response.write('vitalik: ' + requestCount)
+    switch (request.url) {
+        case '/students':
+            response.write('students')
+            break;
+        case '/':
+            case '/courses':
+                response.write('Front + back')
+                break;
+                default:
+                    response.write('404 not found')
+    }
+    
    response.end()
 })
 
